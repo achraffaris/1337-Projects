a3b76f0d7587ddbc04bf4d1f17511eea922e984f
Achraf Faris <ashrafyno2k18@gmail.com>
2022-06-21 16:49:58 +0100

    ok

---
+++ common_core/philosophers/philo/philo.h
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afaris <afaris@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/04 10:09:28 by afaris            #+#    #+#             */
+/*   Updated: 2022/06/20 13:54:01 by afaris           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+#define TRUE 1
+#define FALSE 0
+#define MICROSECOND 1000
+
+typedef struct fork
+{
+    int id;
+    pthread_mutex_t mutex_fork;
+    int status;
+} fork_t;
+
+typedef struct simulation
+{
+    int n_philos;
+    int sleep_time;
+    int eat_time;
+    int die_time;
+    int n_eat;
+    fork_t *forks;
+    pthread_mutex_t mtx_print;
+    pthread_mutex_t mtx_check_death;
+    pthread_mutex_t mtx_check_optional;
+    int all_alive;
+    int n_philos_eated;
+} simulation_t;
+
+typedef struct philo
+{
+    int id;
+    pthread_t th_id;
+    simulation_t *s;
+    fork_t *left_fork;
+    fork_t *right_fork;
+    struct timeval sim_time;
+    int sim_start_ms;
+    int eated_at;
+    pthread_mutex_t mtx_check_death;
+    int n_eat;
+    int completed;
+    int status;
+} philo_t;
+
+
+int     ft_atoi(const char *nptr);
+void    simulation_init(simulation_t *s, char **av);
+void    *simulation(void *philos);
+int     simulation_check(philo_t *ph);
+int     simulation_start(simulation_t *s);
+fork_t  *create_forks(int philos);
+int     current_time_ms();
+void    print_record(char *record, philo_t *ph);
+int     is_alive(philo_t *ph);
+void    is_eating(philo_t *ph);
+
+#endif
---
+++ common_core/philosophers/philo/utils/actions.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afaris <afaris@student.1337.ma>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 16:47:48 by afaris            #+#    #+#             */
+/*   Updated: 2022/06/21 16:47:49 by afaris           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../philo.h"
+
+fork_t  *create_forks(int philos)
+{
+    int i;
+
+    i = 0;
+    fork_t *forks;
+    forks = malloc(sizeof(fork_t) * philos);
+    while (i < philos)
+    {
+        pthread_mutex_init(&forks[i].mutex_fork, NULL);
+        forks[i].status = 1;
+        forks[i].id = i;
+        i++;
+    }
+    return forks;
+}
+
+int     current_time_ms()
+{
+    int now_ms;
+    struct timeval now;
+    
+    now_ms = 0;
+    gettimeofday(&now, NULL);
+    now_ms = (now.tv_sec * MICROSECOND) + (now.tv_usec / MICROSECOND);
+    return (now_ms);
+}
+void    print_record(char *record, philo_t *ph)
+{
+    int start_ms;
+    pthread_mutex_lock(&ph->s->mtx_print);
+    start_ms = ph->sim_start_ms;
+    printf("%dms %d %s\n", current_time_ms()-start_ms ,ph->id, record);
+    pthread_mutex_unlock(&ph->s->mtx_print);
+}
+
+int     is_alive(philo_t *ph)
+{
+    pthread_mutex_lock(&ph->mtx_check_death);
+    if (current_time_ms() >= (ph->eated_at + ph->s->die_time))
+    {
+        print_record("died", ph);
+        pthread_mutex_lock(&ph->s->mtx_print);
+        return (0);
+    }
+    pthread_mutex_unlock(&ph->mtx_check_death);
+    return (1);
+}
+
+void    is_eating(philo_t *ph)
+{
+    print_record("is eating", ph);
+    pthread_mutex_lock(&ph->mtx_check_death);
+    ph->eated_at = current_time_ms();
+    pthread_mutex_unlock(&ph->mtx_check_death);
+    usleep(ph->s->eat_time * MICROSECOND);
+    if (ph->s->n_eat)
+    {
+        ph->n_eat++;
+        if (ph->n_eat >= ph->s->n_eat && !ph->completed)
+        {
+            ph->completed = TRUE;
+            pthread_mutex_lock(&ph->s->mtx_check_optional);
+            ph->s->n_philos_eated++;
+            pthread_mutex_unlock(&ph->s->mtx_check_optional);
+        }
+    }
+    pthread_mutex_unlock(&ph->left_fork->mutex_fork);
+    pthread_mutex_unlock(&ph->right_fork->mutex_fork);
+}
